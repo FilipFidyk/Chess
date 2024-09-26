6bc281656957c9e02043df963c42a3637fccdada
Filip Fidyk <filip.fidyk@wp.pl>
2024-09-26 16:48:32 +0100

    Loaded a piece successfully

--- chessPieces.c
+++ chessPieces.c
@@ -1,10 +1,10 @@
 #include <stdlib.h>
-//#include <stdio.h>
-//#include "chessPieces.h"
+#include <stdio.h>
+#include "chessPieces.h"
 
 //Create the 4 vertices per chess piece, each vertex has 5 attributes hence 4*5*pieces 
 float* createPieceVertices(unsigned int **chessPieces)
-{
+{   
     float *pieceVertices = (float*)calloc(4*5*1, sizeof(float));
 
     unsigned int vertexAttribute = 0;
@@ -12,7 +12,7 @@ float* createPieceVertices(unsigned int **chessPieces)
     {
         for (int j = 0; j<8; j++)
         {
-            if (chessPieces[i][j] == 1)
+            if (chessPieces[i][j])
             {
                 //top left vertex
                 pieceVertices[vertexAttribute++] = -1.0f + (j *  0.25f);
@@ -40,26 +40,30 @@ float* createPieceVertices(unsigned int **chessPieces)
                 pieceVertices[vertexAttribute++] =  1.0f + ((i+1) * -0.25f);
                 pieceVertices[vertexAttribute++] = 0.0f;
                 pieceVertices[vertexAttribute++] = 0.0f;
-                pieceVertices[vertexAttribute++] = 1.0f;
+                pieceVertices[vertexAttribute++] = 0.0f;
             }
         }
     }
+
     return pieceVertices;
-/*
-    for (int i = 0; i < 20; i+=5)
-    {
-        printf ("%d %d %d %d %d\n",pieceVertices[i] ,pieceVertices[i+1] ,pieceVertices[i+2] ,pieceVertices[i+3] ,pieceVertices[i+4]);
-    }*/
 }
 
 //Create the indices for the pieces, 2 triangles per piece, 3 points per triangle, hence 3*2*pieces
+/*
+Chesspiece vertices are generated around the piece in the order: topleft, topright, bottomright, bottomleft
+To create the indices we also chesspiece location:
+    Column coordinate means an offset of a multiple of 4
+    row coordinate means an offset of a multiple of 32
+
+    Then add a value 0,1,2 or 3 to match which corner we want
+*/
 unsigned int* createPieceIndices(unsigned int **chessPieces)
 {
     unsigned int *pieceIndices = (unsigned int*)calloc(3*2*1, sizeof(unsigned int));
 
     unsigned int indicesIndex = 0;
 
-    /*for (int i = 0; i<8;i++)
+    for (int i = 0; i<8;i++)
     {
         for (int j = 0; j<8; j++)
         {
@@ -76,19 +80,11 @@ unsigned int* createPieceIndices(unsigned int **chessPieces)
                 pieceIndices[indicesIndex++] = (32 * i) + (4 * j) + 3;
             }
         }
-    }*/
+    }
 
-    pieceIndices[0] = 0;
-    pieceIndices[1] = 1;
-    pieceIndices[2] = 2;
-    pieceIndices[3] = 0;
-    pieceIndices[4] = 2;
-    pieceIndices[5] = 3;
-/*
-    for (int i = 0; i < 6; i+=3)
+    if (indicesIndex > 3*2*1)
     {
-        printf ("%d %d %d\n",pieceIndices[i] ,pieceIndices[i+1] ,pieceIndices[i+2]);
-    }*/
-
+        printf("INDICES ADDED INCORRECTLY");
+    }
    return pieceIndices;
 }
--- main.c
+++ main.c
@@ -9,7 +9,7 @@
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
-void genVAO(unsigned int *VAO, float *vertices, unsigned long long verticesSize, unsigned int *indices, unsigned long long indicesSize, int texturesVAO);
+void genVAO(unsigned int *VAO, float *vertices, unsigned long long verticesSize, unsigned int *indices, unsigned long long indicesSize, int texturesVAO, unsigned int strideSize);
 
 int main()
 {
@@ -49,11 +49,59 @@ int main()
     float *vertices = createBoardVertices();
     unsigned int *indices = createBoardIndices();
     unsigned int boardVAO;
-    genVAO(&boardVAO, vertices, 3*81*sizeof(float), indices, 3*64*sizeof(unsigned int), 0);
+    genVAO(&boardVAO, vertices, 3*81*sizeof(float), indices, 3*64*sizeof(unsigned int), 0, 3);
     free(vertices);
     free(indices);
 
-    unsigned int **chessPieces = initChessBoard();
+    Shader *shaderPieces;
+    Shader_init(&shaderPieces, "shaders/vPieceShader.txt", "shaders/fPieceShader.txt");
+
+    unsigned int **board = initChessBoard();
+    vertices = createPieceVertices(board);
+    indices = createPieceIndices(board);
+    unsigned int pieceVAO;
+    genVAO(&pieceVAO, vertices, 4*5*1*sizeof(float), indices, 3*2*1*sizeof(unsigned int), 1, 5);
+    glBindVertexArray(pieceVAO);
+    unsigned int texture;
+
+    //Flip the verticality of an image
+    //Images expect the 0.0 y position to be on the top, opengl expects that on the bottom
+    stbi_set_flip_vertically_on_load(1);
+    
+    //TEXTURE 1
+    //Generating the gl texture id
+    glGenTextures(1, &texture);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    //Set the behaviour of the texture sampling in the s,t and r (x,y,z) directions
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);                   
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    
+    //Set the the texture filtering method for scaling up and down, respectively
+    //Mipmaps can only be used in the scaling down
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    
+    //Loading in the image
+    int width1, height1, nrChannels1;
+    unsigned char *data1 = stbi_load("images/chess-pawn.png", &width1, &height1, &nrChannels1, STBI_rgb_alpha); 
+    
+    if (data1)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width1, height1, 0, GL_RGBA, GL_UNSIGNED_BYTE, data1);
+        glGenerateMipmap(GL_TEXTURE_2D);    
+    }
+    else
+    {
+        printf("Failed to load texture");
+    }
+    stbi_image_free(data1);
+
+    glBindVertexArray(pieceVAO);
+    Shader_setInt(&shaderPieces, "texture1", 0);
+
+    
 
     //Polling loop to keep the window running
     while(!glfwWindowShouldClose(window))
@@ -64,6 +112,12 @@ int main()
         glUseProgram(shaderBoard->ID);
         glBindVertexArray(boardVAO);
         glDrawElements(GL_TRIANGLES, 3*64, GL_UNSIGNED_INT, 0);
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+        glUseProgram(shaderPieces->ID);
+        glBindVertexArray(pieceVAO);
+        glDrawElements(GL_TRIANGLES, 3*2*1, GL_UNSIGNED_INT, 0);
+        glDisable(GL_BLEND);
 
         glfwSwapBuffers(window);
         glfwPollEvents();    
@@ -98,7 +152,7 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
     glViewport(0, 0, width, height);
 }
 
-void genVAO(unsigned int *VAO, float *vertices, unsigned long long verticesSize, unsigned int *indices, unsigned long long indicesSize, int texturesVAO)
+void genVAO(unsigned int *VAO, float *vertices, unsigned long long verticesSize, unsigned int *indices, unsigned long long indicesSize, int texturesVAO, unsigned int strideSize)
 {
     unsigned int VBO, EBO;
     glGenVertexArrays(1, VAO);
@@ -113,12 +167,12 @@ void genVAO(unsigned int *VAO, float *vertices, unsigned long long verticesSize,
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, indicesSize, indices, GL_STATIC_DRAW); 
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, strideSize * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
     if (texturesVAO)
     {
-        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)(3*sizeof(float)));
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, strideSize * sizeof(float), (void*)(3*sizeof(float)));
         glEnableVertexAttribArray(1);
     }
 
